38c04c9a79f0d0bf1d1d40db4418eab9e85beb72
Aayush Pal <ap.aayushpal@gmail.com>
2025-01-22 11:17:58 +0530

    Add files via upload

--- threesum.js
+++ threesum.js
@@ -1,49 +1,47 @@
 /**
- * Function threeSum() on a sorted array and we hold the first element
+ * Function threeSum() is called on a sorted array and we hold the first element
  * in place, with two pointers starting from left (ahead of the first) and
  * one from right and use the same logic to round in on the sum that is
  * equal to the one we are given
- * @param {int[]} array
- * @param {int} target
+ * @param {int[]} nums
  */
-
-function threeSum(array, target) {
-  array.sort((a, b) => a - b);
-  const n = array.length;
+function threeSum(nums) {
+  // First we will sort the nums to make it easier to
+  nums.sort((a, b) => a - b);
   const group = new Set();
-  for (let i = 0; i < n - 2; i++) {
-    const first = array[i];
+  const result = [];
 
-    let left = array[i + 1];
-    let right = array[n - 1];
-    let groupKey = "";
-    /**
-     * The requiredSum for the Left and Right can be found using
-     * the complement we get from subtracting the First from the target.
-     * @typedef
-     */
-    const requiredSum = target - first;
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i === 0 && nums[i] === nums[i - 1]) continue;
+
+    let left = i + 1;
+    let right = nums.length - 1;
 
     while (left < right) {
-      if (left + right === requiredSum && right !== left && first !== left) {
-        groupKey = [first, left, right].sort((a, b) => a - b).join(",");
-        if (!group.has(groupKey)) {
-          console.log("Found: ", `${first} + ${left} + ${right}`);
+      const sum = nums[i] + nums[left] + nums[right];
 
+      if (sum === 0) {
+        const groupKey = [nums[i], nums[left], nums[right]]
+          .sort((a, b) => a - b)
+          .join(",");
+
+        if (!group.has(groupKey)) {
+          result.push([nums[i], nums[left], nums[right]]);
           group.add(groupKey);
         }
-        break;
-      } else if (left + right < requiredSum) {
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+
+        left++;
+        right--;
+      } else if (sum < 0) {
         left++;
       } else {
         right--;
       }
     }
   }
+  console.log(result);
 }
 
-const array = [1, 3, -3, 1, -2, 2, -9, 4, 5, 7, 7, -9, -3, 6];
-// const array = [2, 1, 5, 3];
-const target = 0;
-
-threeSum(array, target);
+threeSum([0, 0, 0, 0]);
