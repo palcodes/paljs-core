32571b9a6d92bb361b872fae53983067fa86a11e
Aayush Pal <ap.aayushpal@gmail.com>
2025-01-23 14:18:29 +0530

    Add files via upload

--- linkedlist.js
+++ linkedlist.js
@@ -5,11 +5,13 @@
 
 /**
  * A Node in a LinkedList stores the data and the next pointer.
+ * @constructor
  */
 class LLNode {
-  constructor(data) {
+  constructor(data, random = null) {
     this.data = data;
     this.next = null;
+    this.random = random;
   }
 }
 
@@ -18,12 +20,20 @@ class LLNode {
  * data in an endless memory space by connecting each element in the structure
  * to the next by using a pointer to the next' location.
  *
- * @property head
- * @property size
+ * @class LinkedList
+ * @typedef {Object} LLNode
+ * @property {*} data - The value stored in the node.
+ * @property {LLNode|null} next - The reference to the next node.
+ * @property {LLNode|null} random - The random pointer reference to another node.
+ *
+ * @property {LLNode|null} head - The head node of the linked list.
+ * @property {number} size - The size of the linked list.
  */
 class LinkedList {
   constructor() {
+    /** @type {LLNode|null} */
     this.head = null;
+    /** @type {number} */
     this.size = 0;
   }
 
@@ -32,7 +42,7 @@ class LinkedList {
     if (!this.head) {
       this.head = newNode;
     } else {
-      const current = this.head;
+      let current = this.head;
       while (current.next) {
         current = current.next;
       }
@@ -68,6 +78,31 @@ class LinkedList {
     }
   }
 
+  clone() {
+    if (!this.head) return null;
+
+    const nodeMap = new Map();
+    let current = this.head;
+
+    // Create a copy of each node and store in the Map
+    while (current.next) {
+      nodeMap.set(current, new LLNode(current.data));
+      current = current.next;
+    }
+
+    // Assign `next` and `random` pointers for the copied nodes
+    // The deep part.
+    current = this.head;
+    while (current) {
+      const clonedNode = nodeMap.get(current);
+      clonedNode.next = current.next ? nodeMap.get(current.next) : null;
+      clonedNode.random = current.random ? nodeMap.get(current.random) : null;
+      current = current.next;
+    }
+
+    return nodeMap.get(this.head);
+  }
+
   print() {
     let current = this.head;
     while (current) {
@@ -80,7 +115,17 @@ class LinkedList {
 const list = new LinkedList();
 list.append(1);
 list.append(2);
-list.prepend(0);
-list.prepend(513);
-list.delete(1);
-list.print(); // 0, 1, 2
+list.append(3);
+
+// Setting random pointers
+list.head.random = list.head.next.next; // Node 1 -> Node 3
+list.head.next.random = list.head; // Node 2 -> Node 1
+
+console.log("Original List:");
+list.print();
+
+const clonedList = new LinkedList();
+clonedList.head = list.clone();
+
+console.log("\nCloned List:");
+clonedList.print();
